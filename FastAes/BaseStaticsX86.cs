a07808ec2dce4dbf0bf0c3146cd3e22af84b58b7
John <johnkellyoxford@gmail.com>
2019-03-08 21:48:20 +0000

    Initial commit

---
+++ FastAes/BaseStaticsX86.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+using Vec = System.Runtime.Intrinsics.Vector128<byte>;
+
+[assembly: InternalsVisibleTo("Interactive")]
+
+namespace FastAes
+{
+    internal static unsafe class BaseStaticsX86
+    {
+        private const MethodImplOptions MaxOpt = MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization;
+
+        [MethodImpl(MaxOpt)]
+        public static Vec RawEncryptBlock(Vec block, Span<Vec> keySchedule)
+        {
+            block = Sse2.Xor(block, keySchedule[0]);
+            block = Aes.Encrypt(block, keySchedule[1]);
+            block = Aes.Encrypt(block, keySchedule[2]);
+            block = Aes.Encrypt(block, keySchedule[3]);
+            block = Aes.Encrypt(block, keySchedule[4]);
+            block = Aes.Encrypt(block, keySchedule[5]);
+            block = Aes.Encrypt(block, keySchedule[6]);
+            block = Aes.Encrypt(block, keySchedule[7]);
+            block = Aes.Encrypt(block, keySchedule[8]);
+            block = Aes.Encrypt(block, keySchedule[9]);
+            block = Aes.EncryptLast(block, keySchedule[10]);
+
+            return block;
+        }
+
+        [MethodImpl(MaxOpt)]
+        public static Vec RawDecryptBlock(Vec block, Span<Vec> keySchedule)
+        {
+            block = Sse2.Xor(block, keySchedule[10 + 0]);
+            block = Aes.Decrypt(block, keySchedule[10 + 1]);
+            block = Aes.Decrypt(block, keySchedule[10 + 2]);
+            block = Aes.Decrypt(block, keySchedule[10 + 3]);
+            block = Aes.Decrypt(block, keySchedule[10 + 4]);
+            block = Aes.Decrypt(block, keySchedule[10 + 5]);
+            block = Aes.Decrypt(block, keySchedule[10 + 6]);
+            block = Aes.Decrypt(block, keySchedule[10 + 7]);
+            block = Aes.Decrypt(block, keySchedule[10 + 8]);
+            block = Aes.Decrypt(block, keySchedule[10 + 9]);
+            block = Aes.DecryptLast(block, keySchedule[0]);
+
+            return block;
+        }
+
+        [MethodImpl(MaxOpt)]
+        public static Vector128<byte> KeyExp128(Vec key, byte num)
+        {
+            return InternalKeyExp128(key, Aes.KeygenAssist(key, num));
+        }
+
+        [MethodImpl(MaxOpt)]
+        public static Vector128<byte> InternalKeyExp128(Vec key, Vec keyGenAssisted)
+        {
+            keyGenAssisted = Sse2.Shuffle(keyGenAssisted.As<uint>(), (((3) << 6) | ((3) << 4) | ((3) << 2) | ((3))))
+                .As<byte>();
+            
+            key = Sse2.Xor(key, Aes.ShiftLeftLogical128BitLane(key, 4));
+            key = Sse2.Xor(key, Aes.ShiftLeftLogical128BitLane(key, 4));
+            key = Sse2.Xor(key, Aes.ShiftLeftLogical128BitLane(key, 4));
+
+            return Sse2.Xor(key, keyGenAssisted);
+        }
+
+        [MethodImpl(MaxOpt)]
+        public static void LoadEncryptionKeyOnly(byte* key, Span<Vec> keySchedule)
+        {
+            keySchedule[0] = Sse2.LoadVector128(key);
+            keySchedule[1] = KeyExp128(keySchedule[0], 0x01);
+            keySchedule[2] = KeyExp128(keySchedule[1], 0x02);
+            keySchedule[3] = KeyExp128(keySchedule[2], 0x04);
+            keySchedule[4] = KeyExp128(keySchedule[3], 0x08);
+            keySchedule[5] = KeyExp128(keySchedule[4], 0x10);
+            keySchedule[6] = KeyExp128(keySchedule[5], 0x20);
+            keySchedule[7] = KeyExp128(keySchedule[6], 0x40);
+            keySchedule[8] = KeyExp128(keySchedule[7], 0x80);
+            keySchedule[9] = KeyExp128(keySchedule[8], 0x1B);
+            keySchedule[10] = KeyExp128(keySchedule[9], 0x36);
+        }
+
+        [MethodImpl(MaxOpt)]
+        public static void LoadKey(byte* key, Span<Vec> keySchedule)
+        {
+            LoadEncryptionKeyOnly(key, keySchedule);
+
+            keySchedule[11] = Aes.InverseMixColumns(keySchedule[9]);
+            keySchedule[12] = Aes.InverseMixColumns(keySchedule[8]);
+            keySchedule[13] = Aes.InverseMixColumns(keySchedule[7]);
+            keySchedule[14] = Aes.InverseMixColumns(keySchedule[6]);
+            keySchedule[15] = Aes.InverseMixColumns(keySchedule[5]);
+            keySchedule[16] = Aes.InverseMixColumns(keySchedule[4]);
+            keySchedule[17] = Aes.InverseMixColumns(keySchedule[3]);
+            keySchedule[18] = Aes.InverseMixColumns(keySchedule[2]);
+            keySchedule[19] = Aes.InverseMixColumns(keySchedule[1]);
+        }
+
+        [MethodImpl(MaxOpt)]
+        public static void EncryptBlock(Span<Vec> keySchedule, byte* plainText, byte* cipherTextBuffer)
+        {
+            Vector128<byte> msg = Sse2.LoadVector128(plainText);
+
+            msg = RawEncryptBlock(msg, keySchedule);
+
+            Sse2.Store(cipherTextBuffer, msg);
+        }
+
+        [MethodImpl(MaxOpt)]
+        public static void DecryptBlock(Span<Vec> keySchedule, byte* cipherText, byte* plainTextBuffer)
+        {
+            Vector128<byte> msg = Sse2.LoadVector128(cipherText);
+
+            msg = RawDecryptBlock(msg, keySchedule);
+
+            Sse2.Store(plainTextBuffer, msg);
+        }
+    }
+}
---
+++ Interactive/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Intrinsics;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using static FastAes.BaseStaticsX86;
+
+namespace Interactive
+{
+    public unsafe class Program
+    {
+        private static unsafe void Main(string[] args)
+        {
+            BenchmarkRunner.Run<AesBenchmark>();
+
+            // Test();
+
+            Console.ReadKey();
+        }
+
+        private static void Test()
+        {
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+            byte* truePlainText = stackalloc byte[16];
+            byte* key = stackalloc byte[16];
+
+            byte* cipherText = stackalloc byte[16];
+
+            rng.GetBytes(new Span<byte>(truePlainText, 16));
+            rng.GetBytes(new Span<byte>(key, 16));
+
+            Aes aes = new AesCryptoServiceProvider
+            {
+                Key = new Span<byte>(key, 16).ToArray(),
+                IV = new byte[16],
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.None
+            };
+
+            var stream = new MemoryStream();
+
+            using (var cryptoStream = new CryptoStream(stream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(new ReadOnlySpan<byte>(truePlainText, 16));
+            }
+
+            byte[] encryptedBytes = stream.ToArray();
+
+            Span<Vector128<byte>> keySchedule = new Vector128<byte>[20];
+
+            LoadKey(key, keySchedule);
+            EncryptBlock(keySchedule, truePlainText, cipherText);
+
+            if (encryptedBytes.SequenceEqual(new Span<byte>(cipherText, 16).ToArray()))
+                Console.WriteLine("Good encryption");
+
+            byte* plainText = stackalloc byte[16];
+            LoadKey(key, keySchedule);
+            DecryptBlock(keySchedule, cipherText, plainText);
+
+            stream = new MemoryStream();
+            using (var cryptoStream = new CryptoStream(stream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(new ReadOnlySpan<byte>(cipherText, 16));
+            }
+
+            byte[] decryptedBytes = stream.ToArray();
+
+            if (decryptedBytes.SequenceEqual(new Span<byte>(plainText, 16).ToArray()))
+                Console.WriteLine("Good decryption");
+        }
+
+        [CoreJob(true)]
+        [RPlotExporter]
+        [RankColumn]
+        public class AesBenchmark
+        {
+            private readonly byte[] _truePlainText = { 0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34 };
+            private readonly byte[] _key = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+
+            public AesBenchmark()
+            {
+                _aes = new AesCryptoServiceProvider
+                {
+                    Key = _key,
+                    IV = new byte[16],
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.None
+                }.CreateEncryptor();
+            }
+
+            private readonly ICryptoTransform _aes;
+
+            [Benchmark]
+            public void EncryptIntrinsic()
+            {
+                byte* truePlainText = stackalloc byte[] { 0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34 };
+                byte* key = stackalloc byte[] { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+
+                byte* cipherText = stackalloc byte[16];
+
+                Span<Vector128<byte>> keySchedule = new Vector128<byte>[20];
+
+                LoadKey(key, keySchedule);
+                EncryptBlock(keySchedule, truePlainText, cipherText);
+            }
+
+            [Benchmark]
+            public void EncryptStandardSimple()
+            {
+                var cipherText = new byte[16];
+                _aes.TransformBlock(_truePlainText, 0, 16, cipherText, 0);
+            }
+        }
+    }
+}
